cf6bd8460e105c7127c022fd553d3edda8a73917
Dmitry <dnechv@gmail.com>
2025-09-22 11:35:41 -0700

    added image displaying functionality with extracting pixel bytes per row

--- bmp_reader.py
+++ bmp_reader.py
@@ -5,57 +5,146 @@
 
 #open bmp file
 #bmp image: 0 to 255 bytes containing data 
+def read_bmp(file_path):
+
+    filePath = file_path
 
 
-filePath = "/Users/dnechv/Desktop/bmpPixPeek/input/BIOS.bmp"
+    file = open(filePath, "rb") #rd read in binary
+
+    bmp_bytes = file.read()
+
+    file.close()
+
+    #decimal offset - position of bytes in the bits array 
+    print(int("0x002",16)) #filezie - 2 - size 4 bytes 
+    print(int("0x012",16)) #width - 18 - size 4 bytes
+    print(int("0x016",16)) #height - 22 - size 4 bytes 
+    print(int("0x01C",16)) #bits per pixel - 28 - size 2 bytes 
+    print(int("0x0A",16)) #data offset - where the image data starts - 10 - size 4 bytes 
+
+    #each has position 4 bytes 
+
+    #print(bmp_bytes)
+
+    #extract data - file size, width, height, bits per pixel 
+
+    fileSizeBytes = bmp_bytes[2:6]
+    widthBytes = bmp_bytes[18:22]
+    heightBytes = bmp_bytes[22:26]
+    bitesPerPixelBytes = bmp_bytes[28:30]
+    imageDataOffsetBytes = bmp_bytes[10:14] #where pixel data image starts 
 
 
-file = open(filePath, "rb") #rd read in binary
-
-bmp_bytes = file.read()
-
-file.close()
-
-#decimal offset - position of bytes in the bits array 
-print(int("0x002",16)) #filezie - 2 - size 4 bytes 
-print(int("0x012",16)) #width - 18 - size 4 bytes
-print(int("0x016",16)) #height - 22 - size 4 bytes 
-print(int("0x01C",16)) #bits per pixel - 28 - size 2 bytes 
-print(int("0x0A",16)) #data offset - where the image data starts - 10 - size 4 bytes 
-
-#each has position 4 bytes 
-
-#print(bmp_bytes)
-
-#extract data - file size, width, height, bits per pixel 
-
-fileSizeBytes = bmp_bytes[2:6]
-widthBytes = bmp_bytes[18:22]
-heightBytes = bmp_bytes[22:26]
-bitesPerPixelBytes = bmp_bytes[28:30]
-imageDataOffsetBytes = bmp_bytes[10:14]
-
-
-print(fileSizeBytes)
-print(widthBytes)
-print(heightBytes)
-print(bitesPerPixelBytes)
-
-
-#decode and printdata - file size, width, height, bits per pixel 
-
-image_size = int.from_bytes(fileSizeBytes, 'little')
-image_width = int.from_bytes(widthBytes, 'little')
-image_height = int.from_bytes(heightBytes,'little')
-image_bytes_per_pixel = int.from_bytes(bitesPerPixelBytes,'little')
-image_offset_data = int.from_bytes(imageDataOffsetBytes, 'little')
+    print(fileSizeBytes)
+    print(widthBytes)
+    print(heightBytes)
+    print(bitesPerPixelBytes)
 
 
 
-print(image_size )
-print(image_width)
-print(image_height)
-print(image_bytes_per_pixel)
-print(image_offset_data)
+    #decode and printdata - file size, width, height, bits per pixel 
 
-print("File size: ", image_size, ", bytes width: ",image_width, ", bytes height: ",image_height, ", bytes per pixel: ", image_bytes_per_pixel )
+    image_size = int.from_bytes(fileSizeBytes, 'little')
+    image_width = int.from_bytes(widthBytes, 'little')
+    image_height = int.from_bytes(heightBytes,'little')
+    image_bytes_per_pixel = int.from_bytes(bitesPerPixelBytes,'little')
+    image_offset_data = int.from_bytes(imageDataOffsetBytes, 'little') #where pixel data image starts 
+
+
+
+    print(image_size )
+    print(image_width)
+    print(image_height)
+    print(image_bytes_per_pixel)
+    print(image_offset_data)
+
+
+    print("File size: ", image_size, ", bytes width: ",image_width, ", bytes height: ",image_height, ", bytes per pixel: ", image_bytes_per_pixel )
+
+
+
+    #TODO use pixel_offset, width, height to read one full pixel row and check the first RGB values.
+
+
+    #slicing pixel array to get pixel image data
+
+    pixel_image_data = bmp_bytes [image_offset_data: ]
+
+
+    #number of bytes of pixel color data in one row - RGB - Unpadded 
+    color_bytes_per_row = image_width * 3
+
+    print(color_bytes_per_row)
+
+    #calculate total color data + padding = amount of rows blocks containing image data 
+    row_stride = ((color_bytes_per_row +3)//4) * 4
+
+    print(row_stride)
+
+
+    #reading image data - extract all pixels and store them 
+
+    #starting point for pixel data 
+    pointer = image_offset_data
+
+
+    #TEST - one full block to confirm correct sizing of each row block 
+    one_full_row_block = bmp_bytes[pointer: pointer + row_stride]
+
+    print(len(one_full_row_block))
+
+
+    #container for decoded pixels - each decoded row will be added here 
+    row_pixels_container = []
+
+
+    #loop through all rows 
+
+    for i in range(image_height):
+
+        row_block = bmp_bytes[pointer: pointer + row_stride] #extract one block from the row 
+        pointer += row_stride #move pointer to the next block 
+
+        #to hold row pixel data
+        row_pixel_data = []
+        
+        #loop though the pixels in one block
+        #BGR 
+
+        for x in range (image_width):
+
+            #set the starting pixel
+
+            start = x*3 
+            B = row_block[start]
+            G = row_block[start + 1]
+            R = row_block[start + 2]
+            row_pixel_data.append((R,G,B)) #save pixel data as RGB 
+
+
+        #save the completed row 
+        row_pixels_container.append(row_pixel_data)
+
+
+    #reverse the pixels order so it is stored top to bottom 
+    pixels_rgb = row_pixels_container [::-1]
+
+    return pixels_rgb
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bmp_reader_ui.py
+++ bmp_reader_ui.py
@@ -1,9 +1,58 @@
 import tkinter as tk
 import tkinter.filedialog
-from time import sleep
-from threading import Thread
+import base64
 
 
+
+from bmp_reader import read_bmp
+
+
+#bmp reader ui 
+
+#opens BMP image 
+def open_bmp_image():
+    #get the file path
+    file_path = file_path_entry.get()
+    #get pixels 
+    pixels_rgb = read_bmp(file_path)
+    #fill the pixels 
+
+    #compute the image size
+    height = len(pixels_rgb)
+    width = len(pixels_rgb[0])
+
+    #build header 
+    header_bytes = f"P6\n{width} {height}\n255\n".encode("ascii")
+
+
+    #byte array for the image 
+    all_rgb_bytes = bytearray()
+
+
+    #loop through every row in pixels rgb
+
+    for row in pixels_rgb:
+
+        #loop over each pixel in the row 
+        for (R,G,B) in row:
+
+            all_rgb_bytes.extend([R,G,B])
+    
+  
+    #create a photo image from all bytes 
+    photo = tk.PhotoImage(data = all_bytes, format = "PPM")
+    #display photo in the image label 
+    image_label.configure(image = photo)
+    #store the reference inside the label widget 
+    image_label.image = photo
+
+
+
+
+
+
+
+#file browser 
 def browse_file():
     filepath = tk.filedialog.askopenfilename()
     file_path_entry.delete(0, tk.END)
@@ -18,13 +67,19 @@ file_path_entry.grid(row=0, column=1)
 
 tk.Button(root, text="Browse", command=browse_file).grid(row=1, column=0)
 
+tk.Button(root, text="Open BMP Image", command=open_bmp_image).grid(row=1, column=1)
 
-def start_compression():
-    Thread(target=compress_file).start()
 
-def compress_file():
-    sleep(5)
+#image widget
 
-tk.Button(root, text="Compress", command=start_compression).grid(row=1, column=1)
+image_label = tk.Label(root)
+image_label.grid(row = 2, column = 0, columnspan =2)
+
+
+
+#end of the UI block 
 
 root.mainloop()
+
+
+
