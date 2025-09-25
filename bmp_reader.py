70a5b0658ea5e817d81f0e3c48ba35b0c7d004a5
Dmitry <dnechv@gmail.com>
2025-09-25 09:01:04 -0700

    all bmp images now load + added brigntess slider + ui metadata

--- bmp_reader.py
+++ bmp_reader.py
@@ -72,15 +72,57 @@ def read_bmp(file_path):
     pixel_image_data = bmp_bytes [image_offset_data: ]
 
 
-    #number of bytes of pixel color data in one row - RGB - Unpadded 
-    color_bytes_per_row = image_width * 3
+    #saves images bits per pixel 
+    bpp = image_bytes_per_pixel  
 
-    print(color_bytes_per_row)
+    # calculate row stride 
+    row_stride = ((bpp * image_width + 31) // 32) * 4  
 
-    #calculate total color data + padding = amount of rows blocks containing image data 
-    row_stride = ((color_bytes_per_row +3)//4) * 4
-
+    #degub row stride
     print(row_stride)
+    
+    #BMP 24bpp stores BGR values directly
+    #BMP with 1,4,8 bpp stores index used in pallete to locate RGB color 
+    #palette/colortable - BMP image part stores a lost of RGB Colors 
+    #read palete if index is 1, 4, 8 bpp
+    #each entry = BGRA - 4 bytes - blue, green, red, reserved 
+
+    colorTable = []
+
+    if bpp in (1,4,8):
+        
+        #read dib header 0x0E (14)-> color table starts after 
+        dib_header_size = int.from_bytes(bmp_bytes[14:18], 'little')
+        
+        #calcuate the end of the DIB header 
+        dib_end = 14 + dib_header_size 
+
+        #slice the color table bytes
+        colorTable_bytes = bmp_bytes[dib_end:image_offset_data]
+
+        #calculate number of expect entires in the color table 
+        expected_entries = 2 ** bpp
+
+        #calculate the amount of entries in the color table 
+        entires = len(colorTable_bytes) // 4
+
+        #BMP files can have fewer colors than expected 
+        color_entires = min(entires, expected_entries)
+
+        #go through color enties and extract them 
+        for i in range(color_entires):
+            B = colorTable_bytes[4*i]
+            G = colorTable_bytes[4*i +1]
+            R = colorTable_bytes[4*i +2]
+            colorTable.append((R,G,B))
+
+
+
+
+
+
+
+
 
 
     #reading image data - extract all pixels and store them 
@@ -112,15 +154,87 @@ def read_bmp(file_path):
         #loop though the pixels in one block
         #BGR 
 
+
+        #must be decoded based on bits per pixel
+
+
+        #decoding loop 
         for x in range (image_width):
 
-            #set the starting pixel
+            #decoding the image based on bpp
 
-            start = x*3 
-            B = row_block[start]
-            G = row_block[start + 1]
-            R = row_block[start + 2]
-            row_pixel_data.append((R,G,B)) #save pixel data as RGB 
+
+            if bpp ==24:
+
+                start = x*3 
+                B = row_block[start]
+                G = row_block[start + 1]
+                R = row_block[start + 2]
+                row_pixel_data.append((R,G,B)) #save pixel data as RGB 
+
+            elif bpp == 8:
+                #1 byte per pixel - index in colortable
+
+                #get colortable index from row data
+                index = row_block[x]
+
+                #match index with colortable 
+                color = colorTable[index]
+
+                #add RGB color to row's pixel list
+                row_pixel_data.append(color)
+
+            elif bpp == 4:
+                #2 pixels per byte 
+
+                #calculate byte position for current pixel
+                byte_position = x //2 
+
+                #read byte's value from row data 
+
+                byte_value = row_block[byte_position]
+
+                #check if the pixel is first (even) or second (odd) inside the byte
+
+                if x %2 ==0:
+
+                    #take first high bits 
+                    index = byte_value >> 4 
+                
+                else: 
+
+                    #take 4 low bits
+                    index = byte_value & 0x0F
+
+
+                #record color 
+                color = colorTable[index]
+
+                #add to pixel data 
+                row_pixel_data.append(color)
+
+
+            elif bpp ==1:
+                #8 pixels per byte
+
+                #calculate yte holding the pixel 
+                byte_position = x//8
+
+                #read the byte value 
+                byte_value = row_block[byte_position]
+
+                #calculate which bit to use inside 
+                bit_position = 7 - (x%8)
+
+                #extract the single bit at the given position from the byte 
+                index = (byte_value >> bit_position) & 1
+
+                #find RGB color from Colortable 
+                color = colorTable[index]
+
+                #add to row's pixel data
+                row_pixel_data.append(color)
+
 
 
         #save the completed row 
@@ -130,7 +244,17 @@ def read_bmp(file_path):
     #reverse the pixels order so it is stored top to bottom 
     pixels_rgb = row_pixels_container [::-1]
 
-    return pixels_rgb
+
+    #store metadata in a container 
+
+    image_metadata = {
+        "file_size": image_size,
+        "width" : image_width,
+        "height" : image_height,
+        "bits_per_pixel" : bpp,
+    }
+
+    return pixels_rgb,image_metadata 
 
 
         
--- bmp_reader_ui.py
+++ bmp_reader_ui.py
@@ -3,51 +3,117 @@ import tkinter.filedialog
 import base64
 
 
+from tkinter import messagebox
+
+
 
 from bmp_reader import read_bmp
+
+#holds decoded BMP pixels 
+original_pixels_rgb = None 
+
+
+#file validation function
+def validate_file(bmp_bytes):
+    #bmp file has header 14 bytes that contains signature 2 bytes
+
+    #checks and returns boolea for file signature 
+    return bmp_bytes[:2] == b'BM'
 
 
 #bmp reader ui 
 
 #opens BMP image 
 def open_bmp_image():
+
+    #set python to use original variable defined on top 
+    global original_pixels_rgb
+
     #get the file path
     file_path = file_path_entry.get()
+
+    #open bytes of the image
+    with open(file_path, "rb") as file:
+
+        #read the image bytes
+        bmp_bytes = file.read()
+
+        #validating the bmp bytes
+
+    if not validate_file(bmp_bytes):
+        messagebox.showerror("Text", "Not a BMP File.Please try another file")
+        return
+
     #get pixels 
-    pixels_rgb = read_bmp(file_path)
-    #fill the pixels 
-
-    #compute the image size
-    height = len(pixels_rgb)
-    width = len(pixels_rgb[0])
-
-    #build header 
-    header_bytes = f"P6\n{width} {height}\n255\n".encode("ascii")
-
-
-    #byte array for the image 
-    all_rgb_bytes = bytearray()
-
-
-    #loop through every row in pixels rgb
-
-    for row in pixels_rgb:
-
-        #loop over each pixel in the row 
-        for (R,G,B) in row:
-
-            all_rgb_bytes.extend([R,G,B])
+    pixels_rgb, metadata = read_bmp(file_path)
     
-  
-    #create a photo image from all bytes 
+
+    #save decoded pixels from BMP in the global variable - image data exits between two functions 
+    original_pixels_rgb = pixels_rgb
+
+    #call color changing function - also displays the image 
+    change_color()
+
+
+    #update metadata labels
+
+    file_size_label.config(text=f"File size: {metadata['file_size']} bytes")
+    width_label.config(text=f"Width: {metadata['width']} px")
+    height_label.config(text=f"Height: {metadata['height']} px")
+    bpp_label.config(text=f"Bits per pixel: {metadata['bits_per_pixel']}")
+
+
+
+
+def change_color(*_):
+
+
+    #check if image is loaded
+
+    if original_pixels_rgb is None:
+        return
+    
+    #reads the value from the brightless slider -> scaling factor in the range 0.0 - 1.0
+    factor = brightness_scale.get()/100.0 
+
+    #get height and width of the image from the stored pixel array 
+    h = len(original_pixels_rgb)
+    w = len(original_pixels_rgb[0])
+
+
+    #build PPM image header in bytes for Tkinter
+    header_bytes = f"P6\n{w} {h}\n255\n".encode("ascii")
+
+
+    #holds new pixel data with adjusted brightness 
+    all_rgb_bytes = bytearray()
+    
+    #loop through original pixels 
+    for row in original_pixels_rgb:
+        #adjust the brightness 
+        for (R, G, B) in row:
+            r = min(255, int(R * factor))
+            g = min(255, int(G * factor))
+            b = min(255, int(B * factor))
+
+            #put adjusted pixels in the byte array 
+            all_rgb_bytes.extend((r, g, b))
+
+
+    #combine header data with new pixel data 
+    all_bytes = header_bytes + all_rgb_bytes
+
+
+    #show the image
+
     photo = tk.PhotoImage(data = all_bytes, format = "PPM")
-    #display photo in the image label 
     image_label.configure(image = photo)
-    #store the reference inside the label widget 
     image_label.image = photo
 
 
 
+
+ 
 
 
 
@@ -57,6 +123,12 @@ def browse_file():
     filepath = tk.filedialog.askopenfilename()
     file_path_entry.delete(0, tk.END)
     file_path_entry.insert(0, filepath)
+
+
+
+
+    
+    
 
 root = tk.Tk()
 
@@ -75,6 +147,24 @@ tk.Button(root, text="Open BMP Image", command=open_bmp_image).grid(row=1, colum
 image_label = tk.Label(root)
 image_label.grid(row = 2, column = 0, columnspan =2)
 
+#metadata lables
+file_size_label = tk.Label(root, text="File size: ")
+file_size_label.grid(row=3, column=0, columnspan=2, sticky="w")
+
+width_label = tk.Label(root, text="Width: ")
+width_label.grid(row=4, column=0, columnspan=2, sticky="w")
+
+height_label = tk.Label(root, text="Height: ")
+height_label.grid(row=5, column=0, columnspan=2, sticky="w")
+
+bpp_label = tk.Label(root, text="Bits per pixel: ")
+bpp_label.grid(row=6, column=0, columnspan=2, sticky="w")
+
+#brigtness slider
+brightness_scale = tk.Scale(root, from_=0, to=100, orient="horizontal",
+    label="Brightness (%)", command=change_color)
+brightness_scale.set(100)
+brightness_scale.grid(row=7, column=0, columnspan=2, sticky="we")
 
 
 #end of the UI block 
